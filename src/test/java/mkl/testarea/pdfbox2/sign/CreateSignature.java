6276d5d262b3fb519a80379c41a35a6a17633896
mkl <mkl@wir-sind-cool.org>
2019-11-26 12:05:04 +0100

    Improved CreateSignature.lockFields method
    Improved CreateSignature.lockFields method to not mark fields
    unnecessarily for inclusion in incremental update.

--- src/test/java/mkl/testarea/pdfbox2/sign/CreateSignature.java
+++ src/test/java/mkl/testarea/pdfbox2/sign/CreateSignature.java
@@ -643,20 +643,23 @@ public class CreateSignature
         boolean isUpdated = false;
         if (fields != null) {
             for (PDField field : fields) {
+                boolean isUpdatedField = false;
                 if (shallBeLocked.test(field)) {
                     field.setFieldFlags(field.getFieldFlags() | 1);
                     if (field instanceof PDTerminalField) {
                         for (PDAnnotationWidget widget : ((PDTerminalField)field).getWidgets())
                             widget.setLocked(true);
                     }
-                    isUpdated = true;
+                    isUpdatedField = true;
                 }
                 if (field instanceof PDNonTerminalField) {
                     if (lockFields(((PDNonTerminalField)field).getChildren(), shallBeLocked))
-                        isUpdated = true;
+                        isUpdatedField = true;
                 }
-                if (isUpdated)
+                if (isUpdatedField) {
                     field.getCOSObject().setNeedToBeUpdated(true);
+                    isUpdated = true;
+                }
             }
         }
         return isUpdated;
