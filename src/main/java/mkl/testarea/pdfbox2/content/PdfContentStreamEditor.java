47ec4445a44175ebbde09da1d2661ee9f31382f9
mkl <mkl@wir-sind-cool.org>
2020-01-13 16:18:05 +0100

    Fix PdfContentStreamEditor
    In the EditPageContent test testSortDrawsCengage1 a bug in
    PdfContentStreamEditor was identified - a few OperatorProcessor
    implementations recursively feed other operators by which they are
    defined into the stream engine. For the purpose of editing one has to
    ignore those recursive processing calls which PdfContentStreamEditor did
    not do yet. This is fixed now.

--- src/main/java/mkl/testarea/pdfbox2/content/PdfContentStreamEditor.java
+++ src/main/java/mkl/testarea/pdfbox2/content/PdfContentStreamEditor.java
@@ -129,12 +129,19 @@ public class PdfContentStreamEditor extends PDFGraphicsStreamEngine {
 
     @Override
     protected void processOperator(Operator operator, List<COSBase> operands) throws IOException {
-        nextOperation(operator, operands);
-        super.processOperator(operator, operands);
-        write(replacement, operator, operands);
+        if (inOperator) {
+            super.processOperator(operator, operands);
+        } else {
+            inOperator = true;
+            nextOperation(operator, operands);
+            super.processOperator(operator, operands);
+            write(replacement, operator, operands);
+            inOperator = false;
+        }
     }
 
     final PDDocument document;
     OutputStream replacementStream = null;
     ContentStreamWriter replacement = null;
+    boolean inOperator = false;
 }
