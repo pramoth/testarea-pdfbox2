74d827b5f7a8990239cfcb6bed819e78480955b2
mkl <mkl@wir-sind-cool.org>
2020-07-09 17:58:48 +0200

    Improved SearchSubword test focusing on the stack overflow question
    Search texts and get position in pdf with java
    https://stackoverflow.com/questions/62792228/search-texts-and-get-position-in-pdf-with-java
    
    testVariables - The new third run executing findSubwordsImproved(PDDocument, int, String) in contrast collects all text positions and adds virtual space text positions when writeLineSeparator is called. Thereafter, it looks for matches in the whole collection of text positions. Thus, also the matches spanning lines are found.

--- src/test/java/mkl/testarea/pdfbox2/extract/SearchSubword.java
+++ src/test/java/mkl/testarea/pdfbox2/extract/SearchSubword.java
@@ -10,6 +10,7 @@ import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 import org.apache.pdfbox.text.TextPosition;
+import org.apache.pdfbox.util.Matrix;
 import org.junit.Test;
 
 /**
@@ -20,6 +21,10 @@ public class SearchSubword
     /**
      * <a href="http://stackoverflow.com/questions/35937774/how-to-search-some-specific-string-or-a-word-and-there-coordinates-from-a-pdf-do">
      * How to search some specific string or a word and there coordinates from a pdf document in java
+     * </a>
+     * <br/>
+     * <a href="https://stackoverflow.com/questions/62792228/search-texts-and-get-position-in-pdf-with-java">
+     * Search texts and get position in pdf with java
      * </a>
      * <br/>
      * Variables.pdf
@@ -33,6 +38,13 @@ public class SearchSubword
      * the portions forwarded here "words", cf. the calling method
      * {@link PDFTextStripper#writeLine(List<WordWithTextPositions>)}.
      * </p>
+     * <p>
+     * The third run executing {@link #findSubwordsImproved(PDDocument, int, String)}
+     * in contrast collects all text positions and adds virtual space text positions
+     * when <code>writeLineSeparator</code> is called. Thereafter, it looks for matches
+     * in the whole collection of text positions. Thus, also the matches spanning lines
+     * are found.
+     * </p>
      */
     @Test
     public void testVariables() throws IOException
@@ -43,6 +55,7 @@ public class SearchSubword
             System.out.println("\nVariables.pdf\n-------------\n");
             printSubwords(document, "${var1}");
             printSubwords(document, "${var 2}");
+            printSubwordsImproved(document, "${var 2}");
         }
     }
     
@@ -52,6 +65,24 @@ public class SearchSubword
         for (int page = 1; page <= document.getNumberOfPages(); page++)
         {
             List<TextPositionSequence> hits = findSubwords(document, page, searchTerm);
+            for (TextPositionSequence hit : hits)
+            {
+                if (!searchTerm.equals(hit.toString()))
+                    System.out.printf("  Invalid (%s) ", hit.toString());
+                TextPosition lastPosition = hit.textPositionAt(hit.length() - 1);
+                System.out.printf("  Page %s at %s, %s with width %s and last letter '%s' at %s, %s\n",
+                        page, hit.getX(), hit.getY(), hit.getWidth(),
+                        lastPosition.getUnicode(), lastPosition.getXDirAdj(), lastPosition.getYDirAdj());
+            }
+        }
+    }
+
+    void printSubwordsImproved(PDDocument document, String searchTerm) throws IOException
+    {
+        System.out.printf("* Looking for '%s' (improved)\n", searchTerm);
+        for (int page = 1; page <= document.getNumberOfPages(); page++)
+        {
+            List<TextPositionSequence> hits = findSubwordsImproved(document, page, searchTerm);
             for (TextPositionSequence hit : hits)
             {
                 if (!searchTerm.equals(hit.toString()))
@@ -94,4 +125,56 @@ public class SearchSubword
         stripper.getText(document);
         return hits;
     }
+
+    List<TextPositionSequence> findSubwordsImproved(PDDocument document, int page, String searchTerm) throws IOException
+    {
+        final List<TextPosition> allTextPositions = new ArrayList<>();
+        PDFTextStripper stripper = new PDFTextStripper()
+        {
+            @Override
+            protected void writeString(String text, List<TextPosition> textPositions) throws IOException
+            {
+                allTextPositions.addAll(textPositions);
+                super.writeString(text, textPositions);
+            }
+
+            @Override
+            protected void writeLineSeparator() throws IOException {
+                if (!allTextPositions.isEmpty()) {
+                    TextPosition last = allTextPositions.get(allTextPositions.size() - 1);
+                    if (!" ".equals(last.getUnicode())) {
+                        Matrix textMatrix = last.getTextMatrix().clone();
+                        textMatrix.setValue(2, 0, last.getEndX());
+                        textMatrix.setValue(2, 1, last.getEndY());
+                        TextPosition separatorSpace = new TextPosition(last.getRotation(), last.getPageWidth(), last.getPageHeight(),
+                                textMatrix, last.getEndX(), last.getEndY(), last.getHeight(), 0, last.getWidthOfSpace(), " ",
+                                new int[] {' '}, last.getFont(), last.getFontSize(), (int) last.getFontSizeInPt());
+                        allTextPositions.add(separatorSpace);
+                    }
+                }
+                super.writeLineSeparator();
+            }
+        };
+        
+        stripper.setSortByPosition(true);
+        stripper.setStartPage(page);
+        stripper.setEndPage(page);
+        stripper.getText(document);
+
+        final List<TextPositionSequence> hits = new ArrayList<TextPositionSequence>();
+        TextPositionSequence word = new TextPositionSequence(allTextPositions);
+        String string = word.toString();
+        System.out.printf("  -- %s\n", string);
+
+        int fromIndex = 0;
+        int index;
+        while ((index = string.indexOf(searchTerm, fromIndex)) > -1)
+        {
+            hits.add(word.subSequence(index, index + searchTerm.length()));
+            fromIndex = index + 1;
+        }
+
+        return hits;
+    }
+
 }
