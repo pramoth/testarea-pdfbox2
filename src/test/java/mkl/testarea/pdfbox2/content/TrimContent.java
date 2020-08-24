5a677c12d0a0c25ac7a0060b3c56ba14cd500376
mkl <mkl@wir-sind-cool.org>
2020-08-24 16:22:28 +0200

    New TrimContent test focusing on the stack overflow question
    Crop page with PDFBox
    https://stackoverflow.com/questions/63486940/crop-page-with-pdfbox
    
    testTrimAndCenterCengage1 - This test demonstrates how to trim contents (without using the crop box) and then centering the trimmed contents.

--- src/test/java/mkl/testarea/pdfbox2/content/TrimContent.java
+++ src/test/java/mkl/testarea/pdfbox2/content/TrimContent.java
@@ -51,4 +51,39 @@ public class TrimContent {
         }
     }
 
+    /**
+     * <a href="https://stackoverflow.com/questions/63486940/crop-page-with-pdfbox">
+     * Crop page with PDFBox
+     * </a>
+     * <p>
+     * This test demonstrates how to trim contents (without using the
+     * crop box) and then centering the trimmed contents.
+     * </p>
+     */
+    @Test
+    public void testTrimAndCenterCengage1() throws IOException {
+        PDRectangle box = new PDRectangle(200, 300, 300, 400);
+        try (InputStream resource = getClass().getResourceAsStream("Cengage1.pdf")) {
+            PDDocument document = Loader.loadPDF(resource);
+            for (PDPage page : document.getPages()) {
+                PDRectangle cropBox = page.getCropBox();
+                cropBox = centerBoxAroundBox(box, cropBox.getWidth(), cropBox.getHeight());
+                try (PDPageContentStream canvas = new PDPageContentStream(document, page, AppendMode.APPEND, false, true)) {
+                    canvas.setNonStrokingColor(1);
+                    canvas.addRect(cropBox.getLowerLeftX(), cropBox.getLowerLeftY(), cropBox.getWidth(), cropBox.getHeight());
+                    canvas.addRect(box.getLowerLeftX(), box.getLowerLeftY(), box.getWidth(), box.getHeight());
+                    canvas.fillEvenOdd();
+                }
+                page.setMediaBox(cropBox);
+                page.setCropBox(cropBox);
+            }
+            document.save(new File(RESULT_FOLDER, "Cengage1-TrimmedAndCentered.pdf"));
+        }
+    }
+
+    PDRectangle centerBoxAroundBox(PDRectangle box, float width, float height) {
+        float horitontalMargins = (width - box.getWidth()) / 2;
+        float verticalMargins = (height - box.getHeight()) / 2;
+        return new PDRectangle(box.getLowerLeftX() - horitontalMargins, box.getLowerLeftY() - verticalMargins, width, height);
+    }
 }
